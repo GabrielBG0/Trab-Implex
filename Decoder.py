4c715bb0a00055ca984c1918162c929b2cf4828f
Gabriel Borges Gutierrez <gabriel.bgs00@gmail.com>
2021-06-29 15:30:44 -0300

    graph

---
+++ Decoder.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+class Graph:
+    def __init__(self, vector):
+        self.vector = vector
+
+    def distanceBetweenVertices(v1, v2):
+        # TODO calcula a distancia entre dois vertices
+        return
+
+    def getVerticeCoords(v):
+        # TODO retorna coordenadas de um vertice
+        return
