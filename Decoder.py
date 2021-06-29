88298a64d686fd9dc36ac957f098481d2a3935b6
Gabriel Borges Gutierrez <gabriel.bgs00@gmail.com>
2021-06-29 16:06:36 -0300

    fix

--- Decoder.py
+++ Decoder.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 
 def generateVector(path):
@@ -21,10 +22,18 @@ class Graph:
     def distanceBetweenVertices(self, v1, v2):
         coordsV1 = [self.vector[v1 - 1][1], self.vector[v1 - 1][2]]
         coordsV2 = [self.vector[v2 - 1][1], self.vector[v2 - 1][2]]
-        d = sqrt(((coordsV1[0] - coordsV2[0]) ** 2) +
-                 ((coordsV1[1] - coordsV2[1]) ** 2))
+        d = math.sqrt(((coordsV1[0] - coordsV2[0]) ** 2) +
+                      ((coordsV1[1] - coordsV2[1]) ** 2))
         return d
 
     def getVerticeCoords(self, v):
         # TODO retorna coordenadas de um vertice
         return
+
+
+if __name__ == '__main__':
+    path = 'Dados - Trabalho Final-20210629/att48.tsp.txt'
+
+    graph = createGraph(path)
+
+    print(graph.distanceBetweenVertices(1, 2))
