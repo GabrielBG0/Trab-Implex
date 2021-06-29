4071071b827b2e91813ccdbb710e4e9ba563a39f
Gabriel Borges Gutierrez <gabriel.bgs00@gmail.com>
2021-06-29 16:00:51 -0300

    distancia implementada

--- Decoder.py
+++ Decoder.py
@@ -6,13 +6,24 @@ def generateVector(path):
     return vector
 
 
+def createGraph(path):
+    vector = generateVector(path)
+
+    graph = Graph(vector)
+
+    return graph
+
+
 class Graph:
     def __init__(self, vector):
         self.vector = vector
 
     def distanceBetweenVertices(self, v1, v2):
-        # TODO calcula a distancia entre dois vertices
-        return
+        coordsV1 = [self.vector[v1 - 1][1], self.vector[v1 - 1][2]]
+        coordsV2 = [self.vector[v2 - 1][1], self.vector[v2 - 1][2]]
+        d = sqrt(((coordsV1[0] - coordsV2[0]) ** 2) +
+                 ((coordsV1[1] - coordsV2[1]) ** 2))
+        return d
 
     def getVerticeCoords(self, v):
         # TODO retorna coordenadas de um vertice
